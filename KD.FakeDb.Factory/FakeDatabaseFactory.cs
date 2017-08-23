3fab939ca0f9eed6d158aecc098ed9e11040d83a
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-08-23 21:12:36 +0200

    Added FakeDatabaseFactory for dynamic object creating.

---
+++ KD.FakeDb.Factory/FakeDatabaseFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace KD.FakeDb.Factory
+{
+    /// <summary>
+    /// Static class which is used to create new objects dynamically.
+    /// </summary>
+    public class FakeDatabaseFactory
+    {
+        /// <summary>
+        /// Just to prevent from creating an object of this class.
+        /// </summary>
+        private FakeDatabaseFactory()
+        {
+        }
+
+        /// <summary>
+        /// Returns new instance of <see cref="IFakeDatabase"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static IFakeDatabase NewDatabase()
+        {
+            return new FakeDatabase();
+        }
+    }
+}
