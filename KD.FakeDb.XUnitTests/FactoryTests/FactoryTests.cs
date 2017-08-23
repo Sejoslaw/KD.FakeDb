90cfa78f6f026df66c0121130cde07b1ea3c43ae
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-08-23 21:26:57 +0200

    Added new test to check if Factory creates database correctly.

---
+++ KD.FakeDb.XUnitTests/FactoryTests/FactoryTests.cs
@@ -0,0 +1,15 @@
+﻿using KD.FakeDb.Factory;
+using Xunit;
+
+namespace KD.FakeDb.XUnitTests.FactoryTests
+{
+    public class FactoryTests
+    {
+        [Fact]
+        public void Was_Fake_Database_successfully_created()
+        {
+            var database = FakeDatabaseFactory.NewDatabase();
+            Assert.NotNull(database);
+        }
+    }
+}
